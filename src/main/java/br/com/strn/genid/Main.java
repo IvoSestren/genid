a6403ba54690926b399395db36f10c83c41789a1
Ivo Sestren Junior <ivo.junior@labtrans.ufsc.br>
2019-09-03 10:52:03 -0300

    GenID

---
+++ src/main/java/br/com/strn/genid/Main.java
@@ -0,0 +1,10 @@
+package br.com.strn.genid;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(GenID.generate());
+        }
+    }
+}
