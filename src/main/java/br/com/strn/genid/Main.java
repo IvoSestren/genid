5119c109894715c1c80f6898a85c0dec44e5a9ec
Ivo Sestren Junior <ivo@sestren.com.br>
2019-09-09 14:39:11 -0300

    Melhorias

--- src/main/java/br/com/strn/genid/Main.java
+++ src/main/java/br/com/strn/genid/Main.java
@@ -3,7 +3,7 @@ package br.com.strn.genid;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
             System.out.println(GenID.generate());
         }
     }
